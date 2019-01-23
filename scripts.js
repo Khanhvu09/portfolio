c1817303ab38ac364241846a6552014572d67b1c
Khanh Vu <khanhvu@Khanhs-MacBook-Pro.local>
2019-01-23 10:55:00 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,146 @@
+$('.close').hide()
+$('#about').click(function(){
+    $('#aboutBody').html(`
+    <div id="test">
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+        <h1>Bye</h1>
+    </div>
+    `,
+    $('.close').show(),
+    // $('.main-button').hide()
+    )
+})
+$('#projects').click(function(){
+    console.log('hello')
+    $('#aboutBody').html(
+        `
+        <div id="test2">
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+        </div>
+        `,
+        $('.close').show(),
+        // $('.main-button').hide()
+    )
+})
+$('#contact').click(function(){
+    console.log('hello')
+    $('#aboutBody').html(
+        `
+        <div id="test3">
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+        </div>
+        `,
+        $('.close').show(),
+        // $('.main-button').hide()
+    )
+})
+$('#resume').click(function(){
+    console.log('hello')
+    $('#aboutBody').html(
+        `
+        <div id="test4">
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+            <h1>Hello Again</h1>
+        </div>
+        `,
+        $('.close').show(),
+        // $('.main-button').hide()
+    )
+})
+    $('.close').click(function(){
+        console.log('Hello')
+        $('#aboutBody').html(``)
+        $('.close').hide()
+        $('.main-button').show()
+    })
+    
