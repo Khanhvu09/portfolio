2fe07e6f328758a28fbd7ea919833adcf7cc612a
Khanh Vu <khanhvu@Khanhs-MacBook-Pro.local>
2019-02-09 11:18:54 -0500

    homepage styling

--- scripts.js
+++ scripts.js
@@ -2,12 +2,14 @@ $('.close').hide()
 $('#aboutBody').hide();
 $('#aboutButton').click(function(){
     $('#aboutBody').html(`
-    <div id="aboutMe" class="test">
-        <h1>ABOUT ME</h1>
-        <h4>I'm a Full Stack Developer from Atlanta, GA.</h4>
-        <p>I enjoy problem solving and designing. I strive to provide beautiful, 
-        elegant and efficient code. No matter what language I use.
-        </p>
+    <div id="aboutPageContainer">
+        <div id="aboutPage">
+            <h1>ABOUT ME</h1>
+            <h4>I'm a Full Stack Developer from Atlanta, GA.</h4>
+            <p>I enjoy problem solving and designing. I strive to provide beautiful, 
+            elegant and efficient code. No matter what language I use.
+            </p>
+        </div>
     </div>
     `)
     $('.close').show()
@@ -23,6 +25,14 @@ $('#projectsButton').click(function(){
             <div id="theProffessionalHobo">
                 <div id="TPHName">The Proffessional Hobo</div>
                 <img class="projectImage" id="TPH" src="./images/theProffessionalHobo.png"/>
+                <span class="pageDesc">
+                    <ul>
+                        <li>Description of webpage</li>
+                        <li>Description of webpage</li>
+                        <li>Description of webpage</li>
+                        <li>Description of webpage</li>
+                    </ul>
+                </span>
                 <div id="TPHLinks">
                     <a target="_blank" id="TPHGitHub" href="https://github.com/Khanhvu09/project_front-end">GitHub</a>
                     <a target="_blank" id="TPHDemo" href="./frontEnd/index.html">Live Demo</a>
@@ -31,15 +41,12 @@ $('#projectsButton').click(function(){
             <div id="theBudgetHobo">
                 <div id="TBHName">The Budget Hobo</div>
                 <img class="projectImage" src="./images/theBudgetHobo.png"/>
+                <span class="pageDesc">Description of webpage Description of webpage Description of webpage Description of webpage</span>
                 <div id="TBHLinks">
                     <a target="_blank" id="TBHGitHub" href="https://github.com/Khanhvu09/backendProject">GitHub</a>
                     <a target="_blank" id="TBHDemo" href="http://thebudgethobo.khanhqvu.com">Live Demo</a>
                 </div>
             </div>
-        </div>
-
-        
-        
         </div>
         `)
         $('.close').show(),
@@ -50,7 +57,7 @@ $('#contactButton').click(function(){
     console.log('hello')
     $('#aboutBody').html(
         `
-        <div class="test">
+        <div id="contactPage">
             <form id="contact" method="POST" action="https://formspree.io/kqvu09@gmail.com">
                 <span>Contact Me</span>
                 <input id="yourName" type="text" name="name" placeholder=" Your Name"/>
@@ -69,8 +76,7 @@ $('#resumeButton').click(function(){
     console.log('hello')
     $('#aboutBody').html(
         `
-        <div id="test4" class="test">
-         sapiente quidem, quam, ullam praesentium vel excepturi voluptatem odio sunt optio porro soluta? Laborum corporis eveniet sequi non sed.
+        <div id="resume">
     
         </div>
         `)
@@ -97,6 +103,7 @@ $('#skillsButton').click(function(){
                 <i class="devicon-jquery-plain-wordmark"></i>
                 <i class="devicon-apache-plain-wordmark"></i>
                 <i class="fab fa-node"></i>
+                <i class="devicon-bootstrap-plain-wordmark"></i>
             </div>
             <h3 class="skillsTitle">Skills I'm learning</h3>
             <div id="learningSkills">
