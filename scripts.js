80eae1ea8bda7c1abd2c51e4eaccbd7219bd25db
Khanh Vu <khanhvu@Khanhs-MacBook-Pro.local>
2019-02-11 22:09:47 -0500

    finished about me

--- scripts.js
+++ scripts.js
@@ -19,7 +19,6 @@ function animate2(){
 function animate3(){
     $('#projectsButton').animate({top: '530px', right: '800px'}, 600, 'linear')
     $('#contactButton').animate({left: '1170px', bottom: '550px'}, 600, 'linear')
-    // $('#contactButton').animate({left: '100%', bottom: '500%'}, 600, 'linear')
     $('#aboutButton').animate({right: '640px', bottom: '340px'}, 600, 'linear')
     $('#skillsButton').animate({top: '500px', left: '1030px'}, 600, 'linear')
 }
@@ -29,15 +28,30 @@ $('#aboutButton').click(function(){
     <div id="aboutPageContainer">
         <div id="aboutPage">
             <h1>ABOUT ME</h1>
-            <h4>I'm a Full Stack Developer from Atlanta, GA.</h4>
-            <p>I enjoy problem solving and designing. I strive to provide beautiful, 
-            elegant and efficient code. No matter what language I use.
-            </p>
+            <h3>Hi, I'm Khanh</h3>
+            <p>Born and raised in Orange County, CA. I have been in Atlanta long enough to know
+            not to call it Hot-Lanta. I am a proud, analytical Full-Stack Web Developer who
+            utilizes a dozen different technical skills from HTML to JavaScript.</p>
+            <p>Most of my time is spent at the drawing board coming up with designs for my next project
+            or face down in my Mac debugging or typing out lines of code. If you do not
+            see me there, I am doing 1 of these 3 things:</p>
+            <ol>
+                <li>In my gaming lair... I mean gaming chair.</li>
+                <li>Hitching my mountain bike to my car about to head out to the trails.</li>
+                <li>Restaurant hopping on Buford Hwy.</li>
+            </ol>
+            <p>Discovering my passion for coding did not come to me as quick as others but my love for technology did.
+            My love for tech started when I was 8 when I got my first RC car, tore it apart, just to see how it worked and put it back together
+            again to play with. That's when my family discovered that I was the "techie" to go to for all of the answers.
+            Discovering my passion for coding started with Udemy courses learning this new "hobby" and then I 
+            looked up from my screen and it was 2:00am. This passion of mine has come with a lot of hours spent building websites from scratch, many nights of Chinese take-out and a few all-nighters
+            to perfect the projects while improving my code. Turning a hobby into a career is everyone's dream...I just happened to hustile to make 
+            that a reality.</p>
         </div>
     </div>
     `)
     $('#aboutBody').show()
-    if (parseInt($(window).width()) < 600){
+    if (parseInt($(window).width()) < 812){
         $('.closeMobile').show()
         $('.main-button').hide()
     } else {
@@ -50,17 +64,13 @@ $('#projectsButton').click(function(){
     $('#aboutBody').html(
         `
         <h1 id="projectTitle" >Projects</h1>
-        <div id="projectPage" class="test">
+        <div id="projectPage">
             <div id="theProffessionalHobo">
                 <div id="TPHName">The Proffessional Hobo</div>
                 <img class="projectImage" id="TPH" src="./images/theProffessionalHobo.png"/>
                 <span class="pageDesc">
-                    <ul>
-                        <li>Description of webpage</li>
-                        <li>Description of webpage</li>
-                        <li>Description of webpage</li>
-                        <li>Description of webpage</li>
-                    </ul>
+                    The Proffessional Hobo is a travel web app that utilizes amadeus api to provide
+                    live flight information. 
                 </span>
                 <div id="TPHLinks">
                     <a target="_blank" id="TPHGitHub" href="https://github.com/Khanhvu09/project_front-end">GitHub</a>
@@ -70,7 +80,8 @@ $('#projectsButton').click(function(){
             <div id="theBudgetHobo">
                 <div id="TBHName">The Budget Hobo</div>
                 <img class="projectImage" src="./images/theBudgetHobo.png"/>
-                <span class="pageDesc">Description of webpage Description of webpage Description of webpage Description of webpage</span>
+                <span class="pageDesc">The Budget Hobo allows the user to link their bank accounts with the app
+                to keep track of spending. The app also includes their own personal budget calculator to help budget as well.</span>
                 <div id="TBHLinks">
                     <a target="_blank" id="TBHGitHub" href="https://github.com/Khanhvu09/backendProject">GitHub</a>
                     <a target="_blank" id="TBHDemo" href="http://thebudgethobo.khanhqvu.com">Live Demo</a>
@@ -79,7 +90,7 @@ $('#projectsButton').click(function(){
         </div>
         `)
         $('#aboutBody').show()
-        if (parseInt($(window).width()) < 600){
+        if (parseInt($(window).width()) < 812){
             $('.closeMobile').show()
             $('.main-button').hide()
         } else {
@@ -88,7 +99,6 @@ $('#projectsButton').click(function(){
         }
 })
 $('#contactButton').click(function(){
-    console.log('hello')
     $('#aboutBody').html(
         `
         <div id="contactPage">
@@ -103,7 +113,7 @@ $('#contactButton').click(function(){
         </div>
         `)
         $('#aboutBody').show()
-        if (parseInt($(window).width()) < 600){
+        if (parseInt($(window).width()) < 812){
             $('.closeMobile').show()
             $('.main-button').hide()
         } else {
@@ -120,7 +130,7 @@ $('#resumeButton').click(function(){
         </div>
         `)
         $('#aboutBody').show()
-        if (parseInt($(window).width()) < 600){
+        if (parseInt($(window).width()) < 812){
             $('.closeMobile').show()
             $('.main-button').hide()
         } else {
@@ -159,7 +169,7 @@ $('#skillsButton').click(function(){
         </div>
         `)
         $('#aboutBody').show()
-        if (parseInt($(window).width()) < 600){
+        if (parseInt($(window).width()) < 812){
             $('.closeMobile').show()
             $('.main-button').hide()
         } else {
@@ -170,7 +180,6 @@ $('#skillsButton').click(function(){
     $('.close').click(()=>{
         $('.main-button').show()
         animate2()
-        // $('#aboutBody').hide()
         $('.close').hide()
     })
 
